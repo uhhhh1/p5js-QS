dee877e0cb2d459cf8905d1bc15c790e747db76e
chloe <chloep2838@tuckahoeschools.org>
2025-03-05 13:28:58 +0000

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 function setup(){
-    createCanvas(400, 400);
+    createCanvas(1000, 1000, WEBGL);
 }
 
 function draw(){
     background(255, 0 , 200);
+      // Enable orbiting with the mouse.
+      let options = {
+        disableTouchActions: false,
+        freeRotation: true
+      };
+
+      orbitControl(10, 10, 10, options);
+        // Draw the sphere.
+        
+        box();
+
+
 }
+
+
+
+
